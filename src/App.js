e4896ce39f322842e41bdfaa5271b2f666ec41d4
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-13 09:41:23 +0900

    11.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,61 +1,28 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }){
-  return( 
-  <div>
-    <h2>i like { name }!</h2>
-    <h4>{rating} / 10.0 </h4>
-    <img src={picture} alt={name} />
-  </div>
-  );
-}
+class App extends React.Component{
+  state = {
+    count: 0
+  };
 
+  add = () => {
+    console.log("add");
+  }
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
+  minus = () => {
+    console.log("minus");
+  }
 
-
-const foodILike = [
-    {
-    id:1,
-    name: "Kimchi",
-    image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg",
-    rating: 5
-    },
-    {
-      id:2,
-    name: "bulgogi",
-    image: "https://www.google.com/search?q=%EB%B6%88%EA%B3%A0%EA%B8%B0&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk02ziUIXmsNvR7TuYKh1cpJGpdpH8g:1593509047151&source=lnms&tbm=isch&sa=X&ved=2ahUKEwi-p87lm6nqAhVbUd4KHVSWBBUQ_AUoAXoECBgQAw&biw=1440&bih=701&dpr=2#imgrc=kjZoAmvHGo-KfM",
-    rating:10
-    },
-    {
-      id:3,
-    name: "kimbap",
-    image: "https://www.google.com/search?q=%EA%B9%80%EB%B0%A5&rlz=1C5CHFA_enKR902KR902&sxsrf=ALeKk03dn_nrK6M5JlKKfD5krm8w3qTmGQ:1593509069629&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjqn6rwm6nqAhXDQN4KHYLUA4EQ_AUoAXoECBgQAw&biw=1440&bih=701#imgrc=Xz3xQoYZw3bqEM",
-    rating: 8
-    },
-    {
-      id:4,
-    name: "samgyetang",
-    image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg",
-    rating:7
-    }
-];
-
-function App() {
-  return (
-    <div>
-      <h1>hello!!</h1>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      )
-      )}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>The Number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
