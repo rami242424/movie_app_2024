f2fd59a9eade7aae6e0fcbddabff39121e4f2fbd
Rami <138556019+rami242424@users.noreply.github.com>
2024-04-13 10:51:54 +0900

    12.0 Fetching Movies from API : async await 전

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 class App extends React.Component{
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: [],
   };
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    axios.get("https://yts.mx/api/v2/list_movies.json");
   }
+  // componentDidMount에서 data를 fetch 함 
 
   render() {
     const { isLoading } = this.state;
